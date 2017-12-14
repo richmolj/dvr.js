548827920b81b3b61dbd25fa798dd7943c1ff685
Lee Richmond <lrichmond1@bloomberg.net>
2017-12-14 17:19:20 -0500

    Fix preloader

--- src/nightmare-preload.js
+++ src/nightmare-preload.js
@@ -1,8 +1,10 @@
 let { DVR } = require('dvr-js')
 let fs = require('fs')
 
-const dvrConfig = require('../package.json').dvrConfig
-dvrConfig.series = fs.readFileSync('.dvr.series', 'utf8')
+let dvrConfig = {
+  recordingsFile: 'recordings.json',
+  series: fs.readFileSync('.dvr.series', 'utf8')
+}
 
 dvrConfig.broadcastRecording = function(recording) {
   __nightmare.ipc.send('page', 'saveRecording', recording)
